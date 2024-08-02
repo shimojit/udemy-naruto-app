c488727ffb4ec6fa597eb1f060176073804178f9
shimojit <0liberty4life@gmail.com>
2024-08-02 17:32:40 +0900

    naruto-app 1page completes.

--- src/App.js
+++ src/App.js
@@ -13,27 +13,41 @@ function App() {
 
     const result = await axios.get(apiUrl);
     setCharacters(result.data.characters);
-    console.log(result);
   };
-  return <div className="container">
-    <main>
-      <div className="cards-container">
-        {characters.map((character) => {
-          return <div className='card' key={character.id}>
-            <img
-              src={
-                character.images[0] != null
-                ? character.images[0]
-                : 'dummy.png'
-              }
-              alt="character"
-              className="card-image"
-            />
-          </div>;
-        })}
-      </div>
-    </main>
-  </div>;
+  return (
+    <div className="container">
+      <main>
+        <div className="cards-container">
+          {characters.map((character) => {
+            return (
+              <div className='card' key={character.id}>
+                <img
+                  src={
+                    character.images[0] != null
+                    ? character.images[0]
+                    : 'dummy.png'
+                  }
+                  alt="character"
+                  className="card-image"
+                />
+                <div className="card-content">
+                  <h3 className="card-title">{character.name}</h3>
+                  <p className="card-description">
+                    {character.debut?.appearsIn ?? 'なし'}
+                  </p>
+                  <div className="card-footer">
+                    <span className="affiliation">
+                      {character.personal?.affiliation ?? 'なし'}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
